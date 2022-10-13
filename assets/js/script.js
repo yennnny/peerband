e711e125d1cab9553170a749bc4f2375a7374009
jye104 <jye104@nate.com>
2022-10-13 17:23:36 +0900

    221013 test

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function goBack(){
+  history.back();
+}
+
+// function inputTxtCheck(){
+//   const input = document.querySelectorAll('input')
+//   console.log(input)
+//   input.addEventListener('change', function(){
+//     console.log(this)
+//   });
+// }
+
+// input.addEventListener('change', (ele) => {
+//   console.log(ele)
+//   // input.forEach(ele => {      
+//   //     if(!ele){
+//   //       document.querySelector('.btn_basic').classList.remove('disabled');
+//   //     }       
+//   // });
+// });
