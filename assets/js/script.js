71ab644a6edc9622a34f06eaf1904f1e97742eec
jye104 <jye104@nate.com>
2022-10-17 17:40:38 +0900

    221017 작업

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,18 @@ document.documentElement.style.setProperty('--vh', vh);
 function goBack(){
   history.back();
 }
-document.querySelector('.btn_pop_open').addEventListener('click', function(){
-  const $popup = document.querySelector('.popup');    
-  const $dim = document.createElement('div');
-  $dim.setAttribute('id', 'dim');
-  if(!$popup.classList.contains('on')){
-    !$popup.classList.add('on');
-    document.body.appendChild($dim);
-  }
-  document.querySelector('.btn_confirm').addEventListener('click', () =>{ $dim.remove; });
-});
+if(document.querySelector('.btn_pop_open')){
+  document.querySelector('.btn_pop_open').addEventListener('click', function(){
+    const $popup = document.querySelector('.popup');    
+    const $dim = document.createElement('div');
+    $dim.setAttribute('id', 'dim');
+    if(!$popup.classList.contains('on')){
+      !$popup.classList.add('on');
+      document.body.appendChild($dim);
+    }
+    document.querySelector('.btn_confirm').addEventListener('click', () =>{ $dim.remove; });
+  });
+}
 // function inputTxtCheck(){
 //   const input = document.querySelectorAll('input')
 //   console.log(input)
