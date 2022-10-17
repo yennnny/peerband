9f925716c989b32fe3195561de577b7a0c4e65a7
jye104 <jye104@nate.com>
2022-10-17 15:32:29 +0900

    221017-1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 function goBack(){
   history.back();
 }
-
+document.querySelector('.btn_pop_open').addEventListener('click', function(){
+  const $popup = document.querySelector('.popup');    
+  const $dim = document.createElement('div');
+  $dim.setAttribute('id', 'dim');
+  if(!$popup.classList.contains('on')){
+    !$popup.classList.add('on');
+    document.body.appendChild($dim);
+  }
+  document.querySelector('.btn_confirm').addEventListener('click', () =>{ $dim.remove; });
+});
 // function inputTxtCheck(){
 //   const input = document.querySelectorAll('input')
 //   console.log(input)
@@ -18,3 +27,5 @@ function goBack(){
 //   //     }       
 //   // });
 // });
+
+
